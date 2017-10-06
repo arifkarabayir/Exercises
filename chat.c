63457aaa5a3f639a21c18f3c00cd371d04333e53
Arif Karabayir <arifkarabayir@gmail.com>
2017-10-06 04:33:51 +0300

    Typo edit

--- chat.c
+++ chat.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 {
     if(argc > 2) 
     {
-        printf("Invalid number of argumants\n");
+        printf("Invalid number of arguments\n");
         return 1;
     }
     else if(argc == 2)
